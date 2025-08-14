27365176e34d6705475ec8c3016c4766933f3526
gvnadir <gianlucanadir@gmail.com>
2025-08-15 00:22:37 +0200

    update workspace script

--- .config/set-workspaces.sh
+++ .config/set-workspaces.sh
@@ -16,6 +16,14 @@ done
 # Map Super+0 to workspace 10
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
 
+# Map Super+Shift+1..0 to move windows to workspaces 1..9
+for i in {1..9}; do
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Super><Shift>$i']"
+done
+#
+# Map Super+Shift+0 to move windows to workspace 10
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
+
 # (Optional) Lock workspaces to 10 total instead of dynamic workspaces
 # gsettings set org.gnome.mutter dynamic-workspaces false
 # gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
