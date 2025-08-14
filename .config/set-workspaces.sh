ca80c06d3a869451ae3aa25e452b1212954a58f5
gvnadir <gianlucanadir@gmail.com>
2025-08-14 16:48:02 +0200

    add workspaces script

---
+++ .config/set-workspaces.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Execute this file as a startup program from "Startup applications preferences"
+
+# Disable Super+0..9 for launching dock applications
+for i in {1..9}; do
+  gsettings set org.gnome.shell.keybindings switch-to-application-$i "[]"
+done
+gsettings set org.gnome.shell.keybindings switch-to-application-0 "[]"
+
+# Map Super+1..9 to workspaces 1..9
+for i in {1..9}; do
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
+done
+
+# Map Super+0 to workspace 10
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+
+# (Optional) Lock workspaces to 10 total instead of dynamic workspaces
+# gsettings set org.gnome.mutter dynamic-workspaces false
+# gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
